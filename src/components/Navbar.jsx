6f8e18e118d7d2a338d8ea3dbd77330273ea28f6
fajryalvin12 <fajryalvin12@gmail.com>
2024-07-21 17:14:47 +0700

    feat: create toggle button on Navbar.jsx and NavbarProfile.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useRef } from "react";
 import Logo from "../assets/icons/logo.png";
 import { Link } from "react-router-dom";
 import { FaAlignJustify } from "react-icons/fa6";
 
 function Navbar() {
-  function toggleBar() {}
+  const menuBar = useRef();
+  const authButton = useRef();
+  function toggleBar() {
+    menuBar.current.classList.toggle("hidden");
+    authButton.current.classList.toggle("hidden");
+  }
 
   return (
     <div className="flex flex-col md:flex-row gap-8 md:gap-0 justify-between items-center p-[10px]">
@@ -18,14 +23,20 @@ function Navbar() {
           <FaAlignJustify />
         </button>
       </div>
-      <div className="flex list-none gap-[50px] font-semibold flex-col md:flex-row items-center">
+      <div
+        ref={menuBar}
+        className="flex list-none gap-[50px] font-semibold flex-col md:flex-row items-center"
+      >
         <li className="hover:text-[#3366FF]">Home</li>
         <li className="hover:text-[#3366FF]">
           <Link to={"/CreateEvent"}>Create Event</Link>
         </li>
         <li className="hover:text-[#3366FF]">Location</li>
       </div>
-      <div className="px-[10px] flex gap-[10px] flex-col md:flex-row">
+      <div
+        ref={authButton}
+        className="px-[10px] flex gap-[10px] flex-col md:flex-row"
+      >
         <button className="px-[50px] py-[10px] rounded-xl border font-semibold">
           <Link to={"/Login"}>Login</Link>
         </button>
--- src/components/NavbarProfile.jsx
+++ src/components/NavbarProfile.jsx
@@ -1,26 +1,43 @@
-import React from "react";
+import React, { useRef } from "react";
 import Logo from "../assets/icons/logo.png";
 import AvatarProfile from "../assets/icons/avatar-profile.png";
 import { Link } from "react-router-dom";
+import { FaAlignJustify } from "react-icons/fa6";
 
 function NavbarProfile() {
+  const menuBar = useRef();
+  const profileButton = useRef();
+  function hideBar() {
+    menuBar.current.classList.toggle("hidden");
+    profileButton.current.classList.toggle("hidden");
+  }
+
   return (
     <div className="flex flex-col md:flex-row gap-8 md:gap-0 justify-between items-center p-[10px]">
-      <div>
+      <div className="flex gap-8">
         <div className="flex items-center text-2xl font-bold">
           <img src={Logo} alt="logo" />
           <span className="text-[#3366ff]">We</span>
           <span className="text-[#ff3d71]">tick</span>
         </div>
+        <button onClick={hideBar} className="md:hidden">
+          <FaAlignJustify />
+        </button>
       </div>
-      <div className="flex list-none gap-[50px] font-semibold flex-col md:flex-row items-center">
+      <div
+        ref={menuBar}
+        className="flex list-none gap-[50px] font-semibold flex-col md:flex-row items-center"
+      >
         <li className="hover:text-[#3366FF]">
           <Link to={"/"}>Home</Link>
         </li>
         <li className="hover:text-[#3366FF]">Create Event</li>
         <li className="hover:text-[#3366FF]">Location</li>
       </div>
-      <div className="px-[10px] flex gap-[10px] items-center font-semibold">
+      <div
+        ref={profileButton}
+        className="px-[10px] flex gap-[10px] items-center font-semibold"
+      >
         <div>
           <Link to={"/ProfilePage"}>
             <img
