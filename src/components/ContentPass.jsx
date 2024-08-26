8fa70263b0a18e5dd9d68e34e7df41b47ede36b5
fajryalvin12 <fajryalvin12@gmail.com>
2024-08-26 21:17:36 +0700

    feat:finish feature change password and loading scene

--- src/components/ContentPass.jsx
+++ src/components/ContentPass.jsx
@@ -2,34 +2,24 @@ import React from "react";
 import AvatarProfile from "../assets/icons/avatar-profile.png";
 import Footer from "../components/Footer";
 import { Link } from "react-router-dom";
-import {
-  FaCircleUser,
-  FaCreditCard,
-  FaUserPen,
-  FaLock,
-  FaRectangleList,
-  FaHeart,
-  FaGear,
-  FaArrowRightFromBracket,
-  FaEye,
-} from "react-icons/fa6";
+import { FaEye } from "react-icons/fa6";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import Sidebar from "./Sidebar";
-
+import axios from "axios";
+import Loading from "./Loading";
 
 function ContentPass() {
   const navigate = useNavigate();
-  function clickLogout(e) {
-    e.preventDefault();
-    window.alert("Your account has been logged out!");
-    navigate("/Login");
-  }
-  function clickEdit(e) {
-    e.preventDefault();
-    navigate("/ProfilePage");
-  }
   let [oldPass, setOldPass] = React.useState("password");
+  let [newPass, setNewPass] = React.useState("password");
+  let [confirmPass, setConfirmPass] = React.useState("password");
+  const [message, setMessage] = React.useState("");
+  const [alert, setAlert] = React.useState(0);
+  const [loading, setLoading] = React.useState(0);
+  const profile = useSelector((state) => state.profile.data);
+  const token = useSelector((state) => state.auth.token);
+  const urlChangePass = "http://localhost:8888/users/change-password";
   function oldPassword() {
     if (oldPass === "password") {
       setOldPass("text");
@@ -37,7 +27,6 @@ function ContentPass() {
       setOldPass("password");
     }
   }
-  let [newPass, setNewPass] = React.useState("password");
   function newPassword() {
     if (newPass === "password") {
       setNewPass("text");
@@ -45,7 +34,6 @@ function ContentPass() {
       setNewPass("password");
     }
   }
-  let [confirmPass, setConfirmPass] = React.useState("password");
   function confirmPassword() {
     if (confirmPass === "password") {
       setConfirmPass("text");
@@ -53,27 +41,61 @@ function ContentPass() {
       setConfirmPass("password");
     }
   }
-  const profile = useSelector((state) => state.profile.data);
+  async function changePassword(e) {
+    setLoading(1);
+    e.preventDefault();
+    const oldPass = e.target.oldPass.value;
+    const newPass = e.target.newPass.value;
+    const confirmPass = e.target.confirmPass.value;
+
+    try {
+      const response = await axios.patch(
+        urlChangePass,
+        {
+          oldPassword: oldPass,
+          newPassword: newPass,
+          confirmPassword: confirmPass,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      if (response.data.success) {
+        setMessage(response.data.message);
+        setAlert(1);
+      } else {
+        setMessage(response.data.message);
+        setAlert(1);
+      }
+    } catch (error) {
+      console.error("failed to change password");
+    }
+    setTimeout(() => setLoading(0), 3000);
+  }
 
   return (
     <div className="bg-[#EEEEEE] p-0 md:py-[50px]">
+      {loading ? <Loading /> : ""}
       <div className="flex gap-[20px] mt-12">
         <div className="w-1/3 px-[100px] flex-col gap-[30px] hidden md:flex">
           <Sidebar />
         </div>
         <div className="w-full md:w-2/3 p-0 md:p-[80px] m-0 md:mr-[120px] bg-[#ffff] rounded-[30px] flex flex-col gap-[50px]">
           <div className="font-bold text-2xl">Change Password</div>
-          <form className="flex flex-col gap-[10px]">
+          <form onSubmit={changePassword} className="flex flex-col gap-[10px]">
             <div className="flex justify-between font-semibold items-center gap-12">
-              <label className="w-1/3" for="name">
+              <label className="w-1/3" htmlFor="name">
                 Old Password
               </label>
               <div className="p-[10px] border rounded-xl w-full flex justify-between">
                 <input
                   type={oldPass}
                   placeholder="Input Old Password..."
-                  id="name"
+                  id="oldPass"
                   className="flex-1 h-8 outline-none w-2/3"
+                  name="oldPass"
                 />
                 <button type="button" onClick={oldPassword}>
                   <FaEye />
@@ -81,15 +103,16 @@ function ContentPass() {
               </div>
             </div>
             <div className="flex justify-between font-semibold items-center gap-12">
-              <label className="w-1/3" for="name">
+              <label className="w-1/3" htmlFor="name">
                 New Password
               </label>
               <div className="p-[10px] border rounded-xl w-full flex justify-between">
                 <input
                   type={newPass}
                   placeholder="Input New Password..."
-                  id="name"
+                  id="newPass"
                   className="flex-1 h-8 outline-none w-2/3"
+                  name="newPass"
                 />
                 <button type="button" onClick={newPassword}>
                   <FaEye />
@@ -97,15 +120,16 @@ function ContentPass() {
               </div>
             </div>
             <div className="flex justify-between font-semibold items-center gap-12">
-              <label className="w-1/3" for="name">
+              <label className="w-1/3" htmlFor="name">
                 Confirm Password
               </label>
               <div className="p-[10px] border rounded-xl w-full flex justify-between">
                 <input
                   type={confirmPass}
                   placeholder="Confirm new password..."
-                  id="name"
+                  id="confirmPass"
                   className="flex-1 h-8 outline-none w-2/3"
+                  name="confirmPass"
                 />
                 <button type="button" onClick={confirmPassword}>
                   <FaEye />
@@ -118,6 +142,22 @@ function ContentPass() {
             >
               Update
             </button>
+            {alert ? (
+              <div className="h-12 flex-1 bg-white flex items-center pl-4 justify-between">
+                {message ? (
+                  <button
+                    onClick={() => setAlert(0)}
+                    className="text-green-500"
+                  >
+                    {message}
+                  </button>
+                ) : (
+                  ""
+                )}
+              </div>
+            ) : (
+              ""
+            )}
           </form>
         </div>
       </div>
